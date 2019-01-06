92e50055e200077c86e80f5eddaf7d9528d876d3
EpsilonSequence <miller-94@hotmail.co.uk>
2019-01-06 22:37:56 +0000

    model

---
+++ app/src/main/java/com/example/paycalculator/ShiftModel.java
@@ -0,0 +1,59 @@
+package com.example.paycalculator;
+
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ShiftModel {
+
+    private static ShiftModel sShiftModel;
+
+    private ArrayList<Shift> shiftList;
+
+    public static ShiftModel get(Context context) {
+        if (sShiftModel == null) {
+            sShiftModel = new ShiftModel(context);
+        }
+        return sShiftModel;
+    }
+
+    private ShiftModel(Context context){
+        shiftList = new ArrayList<>();
+
+        // refactor to pattern for data plugins
+        // simulate some data for testing
+
+        for (int i=0; i < 3; i++){
+            Shift shift = new Shift();
+            shift.setShiftTitle("Shift title " + i);
+            //shift.setDetail("Detail for task " + shift.getId().toString());
+
+            shiftList.add(shift);
+        }
+
+    }
+
+    public Shift getShift(UUID todoId) {
+
+        for (Shift shift : shiftList) {
+            if (shift.getmId().equals(todoId)){
+                return shift;
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Shift> getShifts() {
+
+        return shiftList;
+
+    }
+
+    public void addShift(Shift shift){
+
+        shiftList.add(shift);
+
+    }
+
+}
